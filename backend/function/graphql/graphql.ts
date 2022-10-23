3552a63cd7539a79d99dc17eba503653d5cc8132
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-10-22 19:55:39 -0700

    graphql handler

---
+++ backend/function/graphql/graphql.ts
@@ -0,0 +1,26 @@
+import { ApolloServer, gql } from "apollo-server-lambda";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: "bounded",
+  plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+});
+
+export const main = server.createHandler();
