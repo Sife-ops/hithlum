358d688a5a04d139d5224450289b301088133e0a
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-10-26 01:44:53 -0700

    feed type

--- backend/function/graphql/types/query.ts
+++ backend/function/graphql/types/query.ts
@@ -1,7 +1,39 @@
+import { FeedEntityType } from "@hithlum/core/entity/feed";
 import { builder } from "../builder";
+import { hithlumModel } from "@hithlum/core/model";
+
+export const FeedType = builder.objectRef<FeedEntityType>("Feed");
+FeedType.implement({
+  fields: (t) => ({
+    feedId: t.exposeID("feedId"),
+    data: t.exposeString("data"),
+  }),
+});
 
 builder.queryFields((t) => ({
   hello: t.string({
     resolve: () => "hello",
+  }),
+
+  feeds: t.field({
+    type: [FeedType],
+    resolve: async (_, __, { user: { userId } }: any) => {
+      const { data: userFeeds } =
+        await hithlumModel.entities.UserFeedEntity.query.user_({ userId }).go();
+
+      let feeds: FeedEntityType[] = [];
+      for (const userFeed of userFeeds) {
+        const {
+          data: [feed],
+        } = await hithlumModel.entities.FeedEntity.query
+          .feed({
+            feedId: userFeed.feedId,
+          })
+          .go();
+        feeds = [...feeds, feed];
+      }
+
+      return feeds;
+    },
   }),
 }));
