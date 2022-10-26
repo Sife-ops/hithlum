51a15148a2596fcfd581e5ab4f14373a4e15eb08
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-10-26 02:02:10 -0700

    context

--- backend/function/graphql/builder.ts
+++ backend/function/graphql/builder.ts
@@ -1,6 +1,13 @@
 import SchemaBuilder from "@pothos/core";
 
-export const builder = new SchemaBuilder({});
+export const builder = new SchemaBuilder<{
+  Context: {
+    user: {
+      userId: string;
+      email: string;
+    };
+  };
+}>({});
 
 builder.queryType({});
 builder.mutationType({});
