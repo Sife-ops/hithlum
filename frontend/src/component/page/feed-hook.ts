df154ac9aaabf69b3e96ea64b9751394a64e0921
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-10-30 22:45:35 -0700

    feed hook

---
+++ frontend/src/component/page/feed-hook.ts
@@ -0,0 +1,89 @@
+import {
+  Feed as FeedType,
+  useFeedQuery,
+  useSubscribeMutation,
+  useUnsubscribeMutation,
+} from "@hithlum/graphql/urql";
+
+import { graphql } from "@hithlum/graphql/gql";
+import { useEffect, useState } from "react";
+
+export const useFeed = (feedId: string) => {
+  const [feedQueryState] = useFeedQuery({ variables: { feedId: feedId! } });
+  const [feed, setFeed] = useState<FeedType>();
+
+  const [_, subscribeMutation] = useSubscribeMutation();
+  const [__, unsubscribeMutation] = useUnsubscribeMutation();
+
+  useEffect(() => {
+    const { fetching, data } = feedQueryState;
+    if (!fetching && data) {
+      setFeed(data.feed as FeedType);
+    }
+  }, [feedQueryState.data]);
+
+  return {
+    feed,
+    subscribeMutation,
+    unsubscribeMutation,
+  };
+};
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+const subscribe = graphql(`
+  mutation subscribe($feedId: String!) {
+    subscribe(feedId: $feedId) {
+      subscribed
+    }
+  }
+`);
+
+const unsubscribe = graphql(`
+  mutation unsubscribe($feedId: String!) {
+    unsubscribe(feedId: $feedId) {
+      subscribed
+    }
+  }
+`);
+
+const feed = graphql(`
+  query feed($feedId: String!) {
+    feed(feedId: $feedId) {
+      feedId
+      inputUrl
+      private
+      createdAt_isoDate
+
+      feedUrl
+      imageUrl
+      title
+      description
+      link
+
+      subscribed
+
+      articles {
+        articleId
+        feedId
+
+        categories
+        content
+        contentSnippet
+        creator
+        guid
+        isoDate
+        link
+        pubDate
+        summary
+        title
+
+        unread {
+          value
+        }
+      }
+    }
+  }
+`);
--- frontend/src/component/page/feed.tsx
+++ frontend/src/component/page/feed.tsx
@@ -1,52 +1,34 @@
-import {
-  Feed as FeedType,
-  useFeedQuery,
-  useSubscribeMutation,
-  useUnsubscribeMutation,
-} from "@hithlum/graphql/urql";
-import { graphql } from "@hithlum/graphql/gql";
-import { useEffect, useState } from "react";
+import { useFeed } from "./feed-hook";
 import { useParams, Link } from "react-router-dom";
 
 export const Feed = () => {
   const { feedId } = useParams();
-
   // todo: redirect if feedId undefined
-  const [feedQueryState] = useFeedQuery({ variables: { feedId: feedId! } });
-  const [feed, setFeed] = useState<FeedType>();
-
-  const [_, subscribeMutation] = useSubscribeMutation();
-  const [__, unsubscribeMutation] = useUnsubscribeMutation();
-
-  useEffect(() => {
-    const { fetching, data } = feedQueryState;
-    if (!fetching && data) {
-      setFeed(data.feed as FeedType);
-    }
-  }, [feedQueryState.data]);
+  const ctx = useFeed(feedId!);
 
   return (
     <div>
-      {feed && (
+      {ctx.feed && (
         <div>
-          <h3>{feed.title}</h3>
-          <div>{feed.imageUrl}</div>
-          <div>{feed.description}</div>
-          <div>{feed.link}</div>
-          <div>{feed.feedUrl}</div>
+          <h3>{ctx.feed.title}</h3>
+          <div>{ctx.feed.imageUrl}</div>
+          <div>{ctx.feed.description}</div>
+          <div>{ctx.feed.link}</div>
+          <div>{ctx.feed.feedUrl}</div>
           <button
             onClick={() => {
-              const { feedId } = feed;
-              if (feed.subscribed) unsubscribeMutation({ feedId });
-              else subscribeMutation({ feedId });
+              if (!ctx.feed) return;
+              const { feedId } = ctx.feed;
+              if (ctx.feed.subscribed) ctx.unsubscribeMutation({ feedId });
+              else ctx.subscribeMutation({ feedId });
             }}
           >
-            {feed.subscribed ? "unsubscribe" : "subscribe"}
+            {ctx.feed.subscribed ? "unsubscribe" : "subscribe"}
           </button>
 
           <h3>Articles</h3>
           <div>
-            {feed.articles.map((article) => (
+            {ctx.feed.articles.map((article) => (
               <div key={article.articleId}>
                 <div>{article.title}</div>
                 <div>{article.unread.value ? "unread" : "read"}</div>
@@ -64,58 +46,3 @@ export const Feed = () => {
     </div>
   );
 };
-
-const subscribe = graphql(`
-  mutation subscribe($feedId: String!) {
-    subscribe(feedId: $feedId) {
-      subscribed
-    }
-  }
-`);
-
-const unsubscribe = graphql(`
-  mutation unsubscribe($feedId: String!) {
-    unsubscribe(feedId: $feedId) {
-      subscribed
-    }
-  }
-`);
-
-const feed = graphql(`
-  query feed($feedId: String!) {
-    feed(feedId: $feedId) {
-      feedId
-      inputUrl
-      private
-      createdAt_isoDate
-
-      feedUrl
-      imageUrl
-      title
-      description
-      link
-
-      subscribed
-
-      articles {
-        articleId
-        feedId
-
-        categories
-        content
-        contentSnippet
-        creator
-        guid
-        isoDate
-        link
-        pubDate
-        summary
-        title
-
-        unread {
-          value
-        }
-      }
-    }
-  }
-`);
