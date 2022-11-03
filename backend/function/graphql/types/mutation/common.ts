9e2b2ea92f2abb727e88a2db97d68422f3bf2af6
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-11-03 14:09:23 -0700

    make function testable

--- backend/function/graphql/types/mutation/common.ts
+++ backend/function/graphql/types/mutation/common.ts
@@ -1,50 +1,67 @@
 import _ from "lodash";
 import AWS from "aws-sdk";
-// import { ulid } from "ulid";
+import { ulid } from "ulid";
 
 const sqs = new AWS.SQS();
-
 const { ARTICLE_QUEUE } = process.env;
 
 // todo: if batched is too large, switch to one-message strategy
 // todo: increase lambda timeout?
 // todo: remove content until small enough
-const isBodyTooLong = (s: string): boolean => {
-  let bytes = Buffer.byteLength(s, "utf-8");
-  return bytes > 262143;
-};
 
-export const sendArticlesBatch = async (articles: any[], feedId: string) => {
-  const messages = articles.map((article) =>
-    JSON.stringify({ ...article, feedId })
-  );
-  const filtered = messages.filter((message) => !isBodyTooLong(message));
-
-  for (const MessageBody of filtered) {
-    try {
+export const sendArticlesBatch = (articles: any[], feedId: string) => {
+  sendArticlesBatch_(
+    articles,
+    feedId,
+    async (MessageBody) => {
       await sqs
         .sendMessage({
           QueueUrl: ARTICLE_QUEUE!,
           MessageBody,
         })
         .promise();
+    },
+    async (messages) => {
+      await sqs
+        .sendMessageBatch({
+          QueueUrl: ARTICLE_QUEUE!,
+          Entries: messages.map((e: any) => ({
+            Id: ulid(),
+            MessageBody: e,
+          })),
+        })
+        .promise();
+    }
+  );
+};
 
-      // for (const articleChunk of _.chunk(articles, 10)) {
-      //   await sqs
-      //     .sendMessageBatch({
-      //       QueueUrl: ARTICLE_QUEUE!,
-      //       Entries: articleChunk.map((e) => ({
-      //         Id: ulid(),
-      //         MessageBody: JSON.stringify({
-      //           ...e,
-      //           feedId,
-      //         }),
-      //       })),
-      //     })
-      //     .promise();
-      // }
-    } catch (e) {
-      console.log(e);
+export const sendArticlesBatch_ = async (
+  articles: any[],
+  feedId: string,
+  fn1: (MessageBody: any) => Promise<void>,
+  fn2: (messages: any[]) => Promise<void>
+) => {
+  for (const articleChunk of _.chunk(articles, 10)) {
+    let messages: any = [];
+    let bytes = 0;
+    for (const article of articleChunk) {
+      const message = JSON.stringify({ ...article, feedId });
+      messages = [...messages, message];
+      bytes = bytes + Buffer.byteLength(message, "utf-8");
+    }
+
+    if (bytes > 262143) {
+      for (const MessageBody of messages) {
+        bytes = Buffer.byteLength(MessageBody, "utf-8");
+        if (bytes > 262143) {
+          // todo: log this message somewhere
+          continue;
+        } else {
+          await fn1(MessageBody);
+        }
+      }
+    } else {
+      await fn2(messages);
     }
   }
 };
